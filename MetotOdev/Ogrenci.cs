4adca66a5eb6618c8433840df693585b4deaadfb
Kadir Dağlı <mehmetkadirdagli27@gmail.com>
2023-12-28 19:24:27 +0300

    Proje dosyası ekle.

---
+++ MetotOdev/Ogrenci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOdev
+{
+    public class Ogrenci
+    {
+       
+            public void PuanHesapla(string ogrenciAd, string ogrenciSoyad, decimal not1, decimal not2, decimal not3)
+            {
+            Console.WriteLine("ad: {0}, soyad: {1}", ogrenciAd, ogrenciSoyad);
+            decimal ortalama = (not1 + not2 + not3)/3;
+            if (ortalama<45)
+            {
+                Console.WriteLine("ortalama degeriniz: {0} - Kaldınız", ortalama);
+
+            }
+            else
+            {
+                Console.WriteLine("ortalama degeriniz : {0} - Gectiniz!",ortalama);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MetotOdev/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ogrenciAd = string.Empty;
+            string ogrenciSoyad = string.Empty;
+            decimal not1 = 0;
+            decimal not2 = 0;
+            decimal not3 = 0;
+            decimal ortalama = 0;
+            // main metodunda kullanıcıdan adını soyadını ve 3 farklı ders notunu alıcaksın almış
+            // oldugunuz bu bilgileri  ogrencı sınıfı içerisinde ogrenci puanhesapla adında bir metoda
+            // parametre olarak verip daha sonra bilgisini ekrana çıkarıcaksınız daha sonra ortalamasını bulucaksınız 
+            // eger ort 45 ten buyuk ise geçti değil ise kaldı diceksiniz
+            Console.WriteLine("Adınızı Giriniz: ");
+            ogrenciAd = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz: ");
+            ogrenciSoyad = Console.ReadLine();
+            Console.WriteLine("1.notu giriniz: ");
+             not1 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("2.notu giriniz: ");
+             not2 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("3.notu giriniz: ");
+             not3 = decimal.Parse(Console.ReadLine());
+
+            Ogrenci O = new Ogrenci();
+            O.PuanHesapla(ogrenciAd,ogrenciSoyad,not1,not2,not3);
+            
+        }
+    }
+}
